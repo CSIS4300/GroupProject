bbd0f2f2452f777f02b729897c3de426fc47d8d1
ZachChristensen28 <zachchristensen28@gmail.com>
2018-02-23 09:50:33 -0700

    updated flashcard bank to store collections

--- Flashcards/Flashcards/Model/FlashcardBank.swift
+++ Flashcards/Flashcards/Model/FlashcardBank.swift
@@ -11,16 +11,16 @@ import Foundation
 class FlashcardBank {
     
     var list = [Flashcard]()
+    let allCollections = CollectionBank()
     
     init() {
         // Add items to flash cards ## For Temp use ##
         
-        list.append(Flashcard(question: "It is illegal to pee in the Ocean in Portugal", answer: "true", collection: "Econ"))
-        list.append(Flashcard(question: "The color of the sky is..", answer: "blue", collection: "Finance"))
-        list.append(Flashcard(question: "____ affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog", answer: "chocolate", collection: "CSIS 4300"))
-        list.append(Flashcard(question: "The totla surface area of two human lungs is approximately _____", answer: "70 square meters", collection: "Econ"))
-        list.append(Flashcard(question: "Approximately one quarter of human bones are in the feet.", answer: "true", collection: "Finance"))
-        list.append(Flashcard(question: "Google was originally called \"Backrub\"", answer: "true", collection: "CSIS 4300"))
-        
+        list.append(Flashcard(question: "It is illegal to pee in the Ocean in Portugal", answer: "true", collection: allCollections.list[0]))
+        list.append(Flashcard(question: "The color of the sky is..", answer: "blue", collection: allCollections.list[0]))
+        list.append(Flashcard(question: "____ affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog", answer: "chocolate", collection: allCollections.list[0]))
+        list.append(Flashcard(question: "The totla surface area of two human lungs is approximately _____", answer: "70 square meters", collection: allCollections.list[0]))
+        list.append(Flashcard(question: "Approximately one quarter of human bones are in the feet.", answer: "true", collection: allCollections.list[0]))
+        list.append(Flashcard(question: "Google was originally called \"Backrub\"", answer: "true", collection: allCollections.list[0]))
     }
 }
