5db83431301b1336a052b57f6203df3ea5a8693c
ZachChristensen28 <zachchristensen28@gmail.com>
2018-04-19 21:36:59 -0600

    fixed broken commit

--- Flashcards/Flashcards/Controller/FlashcardViewController.swift
+++ Flashcards/Flashcards/Controller/FlashcardViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Carly on 4/19/18.
 //  Copyright © 2018 Carly Chase. All rights reserved.
 //
-
 import UIKit
 
 class FlashcardViewController: UIViewController {
@@ -20,24 +19,23 @@ class FlashcardViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
